657d87bc87adcbe70a47c0cbfffe70fa2acbf742
krystof2ms <61132042@continental.edu.pe>
2025-04-26 18:48:57 -0500

    fix error load other .html

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        acerca: 'acerca-de.html',
+        juegos: 'juegos.html',
+        preventas: 'pre-ventas.html',
+      }
+    }
+  }
 });
