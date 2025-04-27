9f23048c35a5a9f2e42df5efcc88b79d8cafb9a3
krystof2ms <61132042@continental.edu.pe>
2025-04-26 19:31:09 -0500

    fix load html

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        acerca: 'acerca-de.html',
-        juegos: 'juegos.html',
-        preventas: 'pre-ventas.html',
-      }
-    }
+    outDir: "dist", 
   }
 });
